ae6fad63a5eeb5acce9d62664c0bcdf782b893ce
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-06-09 22:58:11 +0900

    main activity

--- bookworms/app/src/main/java/company/co/bookworms/MainActivity.java
+++ bookworms/app/src/main/java/company/co/bookworms/MainActivity.java
@@ -1,14 +1,63 @@
 package company.co.bookworms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private Menu1Fragment menu1Fragment = new Menu1Fragment();
+    private Menu2Fragment menu2Fragment = new Menu2Fragment();
+    private Menu3Fragment menu3Fragment = new Menu3Fragment();
+    private Menu4Fragment menu4Fragment = new Menu4Fragment();
+    ActionBar actionbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        actionbar = getSupportActionBar();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        bottomNavigationView.setSelectedItemId(R.id.main_menu2);
+        actionbar.setTitle("Book Bug");
+        transaction.replace(R.id.frame_layout, menu2Fragment).commitAllowingStateLoss();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.main_menu1: {
+                        transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
+                        actionbar.setTitle("My Book");
+                        break;
+                    }
+                    case R.id.main_menu2: {
+                        transaction.replace(R.id.frame_layout, menu2Fragment).commitAllowingStateLoss();
+                        actionbar.setTitle("Book Bug");
+                        break;
+                    }
+                    case R.id.main_menu3: {
+                        transaction.replace(R.id.frame_layout, menu3Fragment).commitAllowingStateLoss();
+                        actionbar.setTitle("Search");
+                        break;
+                    }
+                    case R.id.main_menu4: {
+                        transaction.replace(R.id.frame_layout, menu4Fragment).commitAllowingStateLoss();
+                        actionbar.setTitle("My Page");
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
     }
 }
